ca1d0d26d44b23520b70191af7729a5707aff93f
player999 <taras.zakharchenko@gmail.com>
2015-08-17 16:35:24 +0300

    Otsu for Array fire does not work

--- src/tests/arrayfire_tests.h
+++ src/tests/arrayfire_tests.h
@@ -47,10 +47,61 @@ public:
     }
 };
 
+template<DeviceType dtype = kOriginal>
+class test_erode_af : PlatformSelector<dtype>, public AFImagePerfTest {
+public:
+    SET_NAME(std::string("Erode 1x13 Arrayfire 3: ") + devices_names[dtype]);
+
+    af::array mask;
+
+    test_erode_af() : AFImagePerfTest(IMWIDTH, IMHEIGHT) {
+        set_sq_side(SQSIDE);
+        set_execution_count(RUN_COUNT);
+        mask =  af::constant(1.0f, 1, 13, f32);
+    }
+
+    void Execute() {
+        wrappedDstImageHost = af::erode(wrappedSrcImageHost, mask);
+    }
+};
+
+template<DeviceType dtype = kOriginal>
+class test_otsu_af : PlatformSelector<dtype>, public AFImagePerfTest {
+public:
+    SET_NAME(std::string("Otsu Arrayfire 3: ") + devices_names[dtype]);
+
+    test_otsu_af() : AFImagePerfTest(IMWIDTH, IMHEIGHT) {
+        set_sq_side(SQSIDE);
+        set_execution_count(RUN_COUNT);
+    }
+
+    void Execute() {
+        // TODO: Does not work
+        unsigned total = wrappedSrcImageHost.elements();
+        af::array hist  = af::histogram(wrappedSrcImageHost, 256, 0.0f, 255.0f);
+        af::array wts   = af::array(af::seq(256));
+        af::array wtB   = af::accum(hist);
+        af::array wtF   = total - wtB;
+        af::array sumB  = af::accum(wts * hist);
+        af::array meanB = sumB / wtB;
+        af::array meanF = (sumB(255) - sumB) / wtF;
+        af::array mDiff = meanB-meanF;
+
+        af::array interClsVar= wtB * wtF * mDiff * mDiff;
+        float max        = af::max<float>(interClsVar);
+        float threshold2 = where(interClsVar == max).scalar<float>();
+        af::array threshIdx  = where(interClsVar >= max);
+        float threshold1 = threshIdx.elements() > 0 ? threshIdx.scalar<float>() : 0.0f;
+
+        wrappedDstImageHost = 255.0f*(wrappedSrcImageHost > ((threshold1 + threshold2) / 2.0f));
+    }
+};
 
 #if defined(AF_ORIGINAL)
-REGISTER_TEST(test_boxfilter_af<kOriginal>);
-REGISTER_TEST(test_resize_af<kOriginal>);
+//REGISTER_TEST(test_boxfilter_af<kOriginal>);
+//REGISTER_TEST(test_resize_af<kOriginal>);
+//REGISTER_TEST(test_erode_af<kOriginal>);
+
 #endif
 
 #if defined(AF_CUDA)
